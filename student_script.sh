5f877fed15c3407c57c82ad3089f1c2ed1bd9702
Hassan Akbad <akbad091@gmail.com>
2025-12-18 20:16:18 +0100

    created stats file with content

--- student_script.sh
+++ student_script.sh
@@ -6,3 +6,7 @@ echo "Utility functions" > src/utils.py
 echo "Configuration" > src/config.py
 cp src/*.py build
 echo "Test cases" > tests/test.py
+touch stats.txt
+ls src | wc -l > stats.txt
+wc -l src/*.py | tail -n 1 >> stats.txt
+wc -w src/config.py >> stats.txt
